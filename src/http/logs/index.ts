58531d5a5ce42675afb7975b7630f1a901755345
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-08 00:09:41 -0300

    feat: add app logs

--- src/http/logs/index.ts
+++ src/http/logs/index.ts
@@ -4,6 +4,10 @@ export function loggerInfo(reply: FastifyReply, description: string) {
   return reply.log.info(description)
 }
 
-export function loggerError(reply: FastifyReply, description: string) {
-  return reply.log.error(description)
+export function loggerError(
+  reply: FastifyReply,
+  description: string,
+  stack?: string,
+) {
+  return reply.log.error({ stack }, description)
 }
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const { PORT } = env
 app
   .listen({
     port: PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.info(`🚀️ HTTP server running at: http://localhost:${PORT}`)
