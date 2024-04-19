b63d1ff2f8fa502c7c89c5814c744dac8f38b4d5
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-18 23:25:22 -0300

    refactor: change user entity

--- src/entities/auth.ts
+++
@@ -1,5 +0,0 @@
-export interface Auth {
-  email?: string
-  cpfCnpj?: string
-  password: string
-}
--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,9 +1,12 @@
-export interface User {
-  id?: string
-  name: string
-  email: string
-  cpfCnpj: string
-  password: string
-  createdAt?: Date
-  updateAt?: Date | null
+export abstract class User {
+  constructor(
+    public id: string,
+    public name: string,
+    public email: string,
+    public cpfCnpj: string,
+    public password: string,
+    public hasActiveSession: boolean = false,
+    public createdAt: Date,
+    public updateAt: Date | null,
+  ) {}
 }
