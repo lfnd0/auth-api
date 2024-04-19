d4f60310c210702330497ad73a952fecf29f72d4
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-04-18 23:56:59 -0300

    refactor: change users error

--- src/use-cases/auth/erros/index.ts
+++ src/use-cases/auth/erros/index.ts
@@ -1,3 +1,9 @@
+export class UserAlreadyExistsError extends Error {
+  constructor() {
+    super('User already exists')
+  }
+}
+
 export class InvalidCredentialsError extends Error {
   constructor() {
     super('Invalid credentials')
