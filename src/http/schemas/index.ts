2953218cba6ed9c363d9aedcc0d750f44c7ba761
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-05-06 22:14:50 -0300

    refactor: change schema validations

--- src/http/schemas/index.ts
+++ src/http/schemas/index.ts
@@ -1,13 +1,24 @@
 import { z } from 'zod'
 import { isValidCpfCnpj } from '../../utils/validators'
 
+const onlyCpfCnpjNumbers = (cpfCnpj: string) => cpfCnpj.replace(/\D/g, '')
+
 export const registerBodySchema = z
   .object({
     name: z.string().min(3, 'Invalid name'),
     email: z.string().email('Invalid e-mail'),
-    cpfCnpj: z.string().refine((cpfCnpjData) => isValidCpfCnpj(cpfCnpjData), {
-      message: 'Invalid cpf or cnpj',
-    }),
+    cpfCnpj: z
+      .string()
+      .refine(
+        (cpfCnpjData) => {
+          const formattedCpfCnpj = onlyCpfCnpjNumbers(cpfCnpjData)
+          return isValidCpfCnpj(formattedCpfCnpj)
+        },
+        {
+          message: 'Invalid CPF or CNPJ',
+        },
+      )
+      .transform(onlyCpfCnpjNumbers),
     password: z.string().min(6, 'Invalid password'),
   })
   .strict()
--- src/utils/validators/index.ts
+++ src/utils/validators/index.ts
@@ -1,6 +1,5 @@
 import { cnpj, cpf } from 'cpf-cnpj-validator'
 
 export function isValidCpfCnpj(cpfCnpj: string) {
-  const onlyCpfCnpjNumbers = cpfCnpj.replace(/\D/g, '')
-  return cpf.isValid(onlyCpfCnpjNumbers) || cnpj.isValid(onlyCpfCnpjNumbers)
+  return cpf.isValid(cpfCnpj) || cnpj.isValid(cpfCnpj)
 }
